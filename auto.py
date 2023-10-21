7ecb12024fa62fef964f0ac736e7ee4df315dac4
Vương Trần Đình Minh <57715612+loversky01@users.noreply.github.com>
2023-10-21 22:41:46 +0700

    update auto run ubuntu

--- auto.py
+++ auto.py
@@ -4,6 +4,7 @@ import subprocess
 while True:
          # Dừng lại trong 1 phút
    
-        subprocess.run(["python", "url.py"])
-        subprocess.run(["python", "2tesst.py"])
+        subprocess.run(["python3", "url.py"])
+        subprocess.run(["python3", "2.py"])
+        subprocess.run(["python3", "tomtat.py"])
         
